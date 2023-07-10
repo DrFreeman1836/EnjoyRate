df14e5031eacd2c2577c338af9d6f1ceacbac64f
Viktor <viktor4gafonov@yandex.ru>
2023-07-10 18:25:17 +0300

    2.0.0 - controllers minor

--- src/main/java/main/rest/Controller.java
+++ src/main/java/main/rest/Controller.java
@@ -2,12 +2,10 @@ package main.rest;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import main.dto.SettingDto;
-import main.dto.SettingPatternsDto;
+import main.dto.RsSignal;
+import main.dto.RsSignal.Signal;
+import main.enam.TypeTrend;
 import main.service.PatternPrice;
 import main.storage.ManagerTicks;
 import main.telegram.impl.TelegramBotMessages;
@@ -15,10 +13,7 @@ import main.storage.impl.TickManagerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,36 +62,35 @@ public class Controller {
   }
 
   @PostMapping("/signal")
-  public ResponseEntity<?> addTick(
+  public ResponseEntity<RsSignal> addTickk(
       @RequestParam(name = "priceAsk") BigDecimal priceAsk,
       @RequestParam(name = "priceBid") BigDecimal priceBid,
-      @RequestParam(name = "pattern") String pattern) {
+      @RequestParam(name = "activity") Boolean activity,
+      @RequestParam(name = "passivity") Boolean passivity,
+      @RequestParam(name = "multi") Boolean multi) {
 
+    RsSignal rsSignal = new RsSignal();
     try {
       tickManagerService.processingTick(priceAsk, priceBid, System.currentTimeMillis());
 
-      if (pattern.equals("activity")) {
+      if (activity) {
         int res = activityPattern.getResponse();
-        if (res != 404) bot.sendMessage(String.valueOf(res) + " новый активный");
-        return ResponseEntity.status(res).build();
+         rsSignal.addSignal("activity", res, TypeTrend.NO_TREND);
       }
-      if (pattern.equals("passivity")) {
+      if (passivity) {
         int res = passivityPattern.getResponse();
-        if (res != 404) bot.sendMessage(String.valueOf(res) + " новый пассивный");
-        return ResponseEntity.status(res).build();
+        rsSignal.addSignal("passivity", res, TypeTrend.NO_TREND);
       }
-      if (pattern.equals("multi")) {
+      if (multi) {
         int res = multiPattern.getResponse();
-        if (res != 404) bot.sendMessage(String.valueOf(res) + " новый мульти");
-        return ResponseEntity.status(res).build();
+        rsSignal.addSignal("multi", res, TypeTrend.NO_TREND);
       }
 
     } catch (Exception ex) {
-      ex.printStackTrace();
-      bot.sendMessage(ex.toString());
+      //bot.sendMessage(ex.toString());
       return ResponseEntity.status(500).build();
     }
-    return ResponseEntity.status(405).build();
+    return ResponseEntity.status(200).body(rsSignal);
   }
 
 }
