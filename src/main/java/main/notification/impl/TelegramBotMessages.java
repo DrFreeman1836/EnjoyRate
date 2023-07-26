a1fe4e79ea4abb3605d20aacac9f0966eddd1a77
Viktor <viktor4gafonov@yandex.ru>
2023-07-26 19:02:30 +0300

    async not tg is done

--- src/main/java/main/notification/impl/TelegramBotMessages.java
+++ src/main/java/main/notification/impl/TelegramBotMessages.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +23,7 @@ public class TelegramBotMessages {
     this.bot = bot;
   }
 
+  @Async
   public void sendMessage(String text) {
     try {
       bot.sendMessage(chatId, text);
